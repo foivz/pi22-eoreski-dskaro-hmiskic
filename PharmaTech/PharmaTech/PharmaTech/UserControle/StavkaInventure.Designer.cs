67c66d84fd2bdbda319fb632a0101b921a47c0ba
hmiskic <101432862+hmiskic@users.noreply.github.com>
2022-05-08 17:40:06 +0200

    Add files via upload
    inventura

---
+++ PharmaTech/PharmaTech/PharmaTech/UserControle/StavkaInventure.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace PharmaTech.UserControle
+{
+    partial class StavkaInventure
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.uxIzbrisi = new System.Windows.Forms.Button();
+            this.uxRazlika = new System.Windows.Forms.TextBox();
+            this.uxStanjeNaSkladistu = new System.Windows.Forms.TextBox();
+            this.uxKolicinaUSustavu = new System.Windows.Forms.TextBox();
+            this.uxNaziv = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(529, 26);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 20);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Razlika";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(353, 26);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 20);
+            this.label3.TabIndex = 23;
+            this.label3.Text = "Stanje u skladištu";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(193, 26);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 20);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Količina u sustavu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 20);
+            this.label1.TabIndex = 21;
+            this.label1.Text = "Naziv artikla";
+            // 
+            // uxIzbrisi
+            // 
+            this.uxIzbrisi.BackColor = System.Drawing.Color.Red;
+            this.uxIzbrisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxIzbrisi.Location = new System.Drawing.Point(697, 45);
+            this.uxIzbrisi.Margin = new System.Windows.Forms.Padding(4);
+            this.uxIzbrisi.Name = "uxIzbrisi";
+            this.uxIzbrisi.Size = new System.Drawing.Size(39, 28);
+            this.uxIzbrisi.TabIndex = 20;
+            this.uxIzbrisi.Text = "X";
+            this.uxIzbrisi.UseVisualStyleBackColor = false;
+            // 
+            // uxRazlika
+            // 
+            this.uxRazlika.Cursor = System.Windows.Forms.Cursors.Help;
+            this.uxRazlika.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxRazlika.Location = new System.Drawing.Point(533, 49);
+            this.uxRazlika.Margin = new System.Windows.Forms.Padding(4);
+            this.uxRazlika.Name = "uxRazlika";
+            this.uxRazlika.Size = new System.Drawing.Size(132, 23);
+            this.uxRazlika.TabIndex = 19;
+            this.uxRazlika.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // uxStanjeNaSkladistu
+            // 
+            this.uxStanjeNaSkladistu.Location = new System.Drawing.Point(357, 49);
+            this.uxStanjeNaSkladistu.Margin = new System.Windows.Forms.Padding(4);
+            this.uxStanjeNaSkladistu.Name = "uxStanjeNaSkladistu";
+            this.uxStanjeNaSkladistu.Size = new System.Drawing.Size(151, 22);
+            this.uxStanjeNaSkladistu.TabIndex = 18;
+            this.uxStanjeNaSkladistu.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // uxKolicinaUSustavu
+            // 
+            this.uxKolicinaUSustavu.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.uxKolicinaUSustavu.Location = new System.Drawing.Point(197, 49);
+            this.uxKolicinaUSustavu.Margin = new System.Windows.Forms.Padding(4);
+            this.uxKolicinaUSustavu.Name = "uxKolicinaUSustavu";
+            this.uxKolicinaUSustavu.Size = new System.Drawing.Size(144, 22);
+            this.uxKolicinaUSustavu.TabIndex = 17;
+            this.uxKolicinaUSustavu.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // uxNaziv
+            // 
+            this.uxNaziv.FormattingEnabled = true;
+            this.uxNaziv.Location = new System.Drawing.Point(24, 48);
+            this.uxNaziv.Margin = new System.Windows.Forms.Padding(4);
+            this.uxNaziv.Name = "uxNaziv";
+            this.uxNaziv.Size = new System.Drawing.Size(160, 24);
+            this.uxNaziv.TabIndex = 16;
+            // 
+            // StavkaInventure
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.uxIzbrisi);
+            this.Controls.Add(this.uxRazlika);
+            this.Controls.Add(this.uxStanjeNaSkladistu);
+            this.Controls.Add(this.uxKolicinaUSustavu);
+            this.Controls.Add(this.uxNaziv);
+            this.Name = "StavkaInventure";
+            this.Size = new System.Drawing.Size(778, 107);
+            this.Load += new System.EventHandler(this.StavkaInventure_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button uxIzbrisi;
+        private System.Windows.Forms.TextBox uxRazlika;
+        private System.Windows.Forms.TextBox uxStanjeNaSkladistu;
+        private System.Windows.Forms.TextBox uxKolicinaUSustavu;
+        private System.Windows.Forms.ComboBox uxNaziv;
+    }
+}
